6eda65214e80a6c449e5a8ac724cc045639c2c03
J-Mad-08 <maddox.version1@gmail.com>
2022-05-12 12:55:28 -0400

    returned commented out code

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 require("dotenv").config();
-console.log(process.env.DATABASE_URL);
 
 const fs = require("fs");
 
@@ -25,32 +24,32 @@ const client = new MongoClient(process.env.DATABASE_URL, {
 });
 // console.log(client);
 
-// client.connect(async (err) => {
-// 	const collection = client.db('BrailleApp').collection('Cards');
-// 	// perform actions on the collection object
-// 	let i = 0;
-// 	try {
-// 		const insertDataFromArray = async function () {
-// 			while (i < arrOfFiles.length) {
-// 				const insertDataFromPhotos = {};
+client.connect(async (err) => {
+  const collection = client.db("BrailleApp").collection("Cards");
+  // perform actions on the collection object
+  let i = 0;
+  try {
+    const insertDataFromArray = async function () {
+      while (i < arrOfFiles.length) {
+        const insertDataFromPhotos = {};
 
-// 				const data = fs.readFileSync(`../public/images/${arrOfFiles[i]}`);
-// 				// console.log(data, 'data');
-// 				insertDataFromPhotos.bin = Binary(data);
+        const data = fs.readFileSync(`../public/images/${arrOfFiles[i]}`);
+        // console.log(data, 'data');
+        insertDataFromPhotos.bin = Binary(data);
 
-// 				await collection.insertOne({
-// 					char: arrOfFiles[i][0],
-// 					insertDataFromPhotos,
-// 				});
-// 				// console.log(insertDataFromPhotos.bin, 'bin');
-// 				delete insertDataFromPhotos.bin;
-// 				++i;
-// 			}
-// 			if (i === arrOfFiles.length) client.close();
-// 		};
-// 		insertDataFromArray();
-// 	} catch (e) {
-// 		console.trace(e);
-// 		client.close();
-// 	}
-// });
+        await collection.insertOne({
+          char: arrOfFiles[i][0],
+          insertDataFromPhotos,
+        });
+        // console.log(insertDataFromPhotos.bin, 'bin');
+        delete insertDataFromPhotos.bin;
+        ++i;
+      }
+      if (i === arrOfFiles.length) client.close();
+    };
+    insertDataFromArray();
+  } catch (e) {
+    console.trace(e);
+    client.close();
+  }
+});
