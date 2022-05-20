1655b6074de200edf14ef52f850e31cf70dd3ab1
J-Mad-08 <maddox.version1@gmail.com>
2022-05-19 23:28:56 -0400

    updated header component

--- src/BrailleCard.jsx
+++ src/BrailleCard.jsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import { Button, Card, CardMedia } from '@mui/material';
+import React from "react";
+import { Button, Card, CardMedia } from "@mui/material";
+import Counter from "./Counter";
+import ConstantStaticRender from "./ConstantStaticRender";
 // should be mapped
 export default function BrailleCard() {
-	function nextCardClick() {
-		console.log('a click');
-		// fetch another card from the db
-	}
+  function nextCardClick(e) {
+    e.preventDefault();
+    console.log("next button clicked");
+    // fetch another card from the db
+  }
 
-	return (
-		// center the styling
+  function prevCardClick(e) {
+    e.preventDefault();
+    console.log("prev button clicked");
+  }
 
-		<Card sx={{ maxWidth: 345 }}>
-			<CardMedia
-				component='img'
-				height='140'
-				alt='North American Braille alphabet'
-				image='static/images/A.jpg'
-			/>
-			A braille card
-			<Button onClick={nextCardClick}>Next card</Button>
-		</Card>
-	);
+  return (
+    // center the styling
+    <>
+      <ConstantStaticRender />
+      <Card sx={{ maxWidth: 500 }}>
+        <CardMedia
+          component="img"
+          height="500"
+          alt="North American Braille alphabet"
+          // had to require image in. Material UI is Not as intuitive as originally believed
+          image={require("./images/A.jpg")}
+          // className="center-Card"
+        />
+        {/* <img className="" src={require("./images/A.jpg")} />A braille card */}
+        {/* form as another component? */}
+        <form>
+          Enter the corresponding English alphabetic character <input />
+        </form>
+        <Button className="button-prev" onClick={prevCardClick}>
+          Previous card
+        </Button>
+        <Button className="button-Next" onClick={nextCardClick}>
+          Next card
+        </Button>
+      </Card>
+      <Counter />
+    </>
+  );
 }
--- src/ConstantStaticRender.jsx
+++ src/ConstantStaticRender.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 const br = require("braille");
-import "../src/styles.scss";
 
 export default function ConstantStaticRender() {
   return (
     <div>
       <header className="App-Header">
-        <h2>{br.toBraille("Flashy Braille")}</h2>
+        <h2>{br.toBraille("Braille Card")}</h2>
       </header>
-      <main>
-        <div className="image-container"></div>
-        <h1>Flashy Braille </h1>
-      </main>
+      <main></main>
     </div>
   );
 }
